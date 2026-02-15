e178ff43f9eef4af1e6f32388dffc8c5a208314f
kvg8378 <kvg8378@nyu.edu>
2026-02-14 21:06:50 -0700

    Add files via upload

---
+++ webServer.py
@@ -0,0 +1,63 @@
+# import socket module
+from socket import *
+# In order to terminate the program
+import sys
+
+
+def webServer(port=13331):
+  serverSocket = socket(AF_INET, SOCK_STREAM)
+  
+  # Prepare a server socket
+  serverSocket.bind(("", port))
+  serverSocket.listen(1)
+
+  while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    
+    try:
+      message = connectionSocket.recv(1024).decode()
+      filename = message.split()[1]
+      
+      # Open the requested file in binary mode
+      f = open(filename[1:], "rb")
+      
+      # Status line (200 OK)
+      outputdata = b"HTTP/1.1 200 OK\r\n"
+              
+      # Content-Type header
+      outputdata += b"Content-Type: text/html; charset=UTF-8\r\n"
+
+      # End of headers
+      outputdata += b"\r\n"
+               
+      # Append file contents
+      for line in f:
+        outputdata += line
+        
+      f.close()
+        
+      # Send everything in ONE send command
+      connectionSocket.send(outputdata)
+        
+      connectionSocket.close()
+      
+    except Exception as e:
+      # 404 Not Found response
+      outputdata = b"HTTP/1.1 404 Not Found\r\n"
+      outputdata += b"Content-Type: text/html; charset=UTF-8\r\n"
+      outputdata += b"\r\n"
+      outputdata += b"<html><body><h1>404 Not Found</h1></body></html>"
+
+      connectionSocket.send(outputdata)
+
+      # Close client socket
+      connectionSocket.close()
+
+  # DO NOT uncomment these for submission
+  #serverSocket.close()
+  #sys.exit()
+
+if __name__ == "__main__":
+  webServer(13331)
